d9a507cb216c6b46e7fb685d1876c9ab9a14c924
Baranowski2288 <baranowski2288@ultra60.mat.umk.pl>
2025-11-22 17:58:18 +0100

    silnia

--- program.cpp
+++ program.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 
+long long silnia(int n){
+	if (n<=1) return 1;
+	return n*silnia(n-1);
+
+}
 int main(){
-	int x;
-	cin>>x;
-	cout<<x<<endl;
+	int n;
+	cin>>n;
+	cout<<silnia(n)<<endl;
 	return 0;
 }
